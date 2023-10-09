0a6c4feb28dad62b48d68c9268183ec39bf1ebb6
Chimaobi Chinaka <Chimaobi01@gmail.com>
2023-10-09 14:38:27 +0100

    runs with host port or port 3001

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const requestLogger = (request, response, next) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
